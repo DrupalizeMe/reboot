b979621b8e0ee940e84a9b5e8e4c8a7b69f1d8e0
Joe Shindelar <eojthebrave@gmail.com>
2021-02-24 11:04:17 -0600

    Add example of using Twig's include feature to create components.

---
+++ templates/components/button/button.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * JavaScript for button component.
+ */
+
+(function ($, window, Drupal) {
+  Drupal.behaviors.rebootButton = {
+    attach: function attach(context, settings) {
+
+      console.log('Button component JS loaded');
+
+    }
+  }
+})(jQuery, window, Drupal);
